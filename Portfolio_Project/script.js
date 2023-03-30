5e0683a6541f3bcebb41fde6f2569aa73b500679
debroy85 <111646293+debroy85@users.noreply.github.com>
2023-03-30 17:29:55 +0530

    Add files via upload
    initial commit

---
+++ Portfolio_Project/script.js
@@ -0,0 +1,16 @@
+console.log("Script Running...")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.menu').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarDisappear')
+    if(document.querySelector('.sidebar').classList.contains('sidebarDisappear')){
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(()=> {
+            document.querySelector('.cross').style.display = 'inline'
+        }, 450)
+        
+    }
+})
